3d4cf23c13c3f2281c1342ac64b6fdc4ea9a1280
Graham <grahamfalconer@icloud.com>
2020-08-27 17:01:39 +0100

    conditional added to keyReleased for black key backgrounds

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ initializePiano = function(){
   attachEventListeners();
 }
 findComponents = function(){
+  // white keys
   a3 = {appearance: document.getElementById('key-a-3'), sound: document.getElementById('A3')}
   b3 = {appearance: document.getElementById('key-b-3'), sound: document.getElementById('B3')}
   c3 = {appearance: document.getElementById('key-c-3'), sound: document.getElementById('C3')}
@@ -17,6 +18,19 @@ findComponents = function(){
   e4 = {appearance: document.getElementById('key-e-4'), sound: document.getElementById('E4')}
   f4 = {appearance: document.getElementById('key-f-4'), sound: document.getElementById('F4')}
   g4 = {appearance: document.getElementById('key-g-4'), sound: document.getElementById('G4')}
+
+  //black keys
+  a3sharp = {appearance:document.getElementById('key-a-3sharp'), sound: document.getElementById('A3sharp')}
+  b3sharp = {appearance:document.getElementById('key-b-3sharp'), sound: document.getElementById('B3sharp')}
+  c3sharp = {appearance:document.getElementById('key-c-3sharp'), sound: document.getElementById('C3sharp')}
+  d3sharp = {appearance:document.getElementById('key-d-3sharp'), sound: document.getElementById('D3sharp')}
+  f3sharp = {appearance:document.getElementById('key-f-3sharp'), sound: document.getElementById('F3sharp')}
+  g3sharp = {appearance:document.getElementById('key-g-3sharp'), sound: document.getElementById('G3sharp')}
+  c4sharp = {appearance:document.getElementById('key-c-4sharp'), sound: document.getElementById('C4sharp')}
+  d4sharp = {appearance:document.getElementById('key-d-4sharp'), sound: document.getElementById('D4sharp')}
+  f4sharp = {appearance:document.getElementById('key-f-4sharp'), sound: document.getElementById('F4sharp')}
+  g4sharp = {appearance:document.getElementById('key-g-4sharp'), sound: document.getElementById('G4sharp')}
+
   discoButton = document.getElementsByClassName('disco')[0]
 }
 attachEventListeners = function(){
@@ -48,6 +62,26 @@ attachEventListeners = function(){
       keyPressed(f4)
     }else if(key == 'Enter'){
       keyPressed(g4)
+    }else if(key == '1'){
+      keyPressed(a3sharp)
+    }else if(key == '2'){
+      keyPressed(b3sharp)
+    }else if(key == '3'){
+      keyPressed(c3sharp)
+    }else if(key == '4'){
+      keyPressed(d3sharp)
+    }else if(key == '6'){
+      keyPressed(f3sharp)
+    }else if(key == '7'){
+      keyPressed(g3sharp)
+    }else if(key == '8'){
+      keyPressed(c4sharp)
+    }else if(key == '9'){
+      keyPressed(d4sharp)
+    }else if(key == '0'){
+      keyPressed(f4sharp)
+    }else if(key == '-'){
+      keyPressed(g4sharp)
     }
   })
   document.addEventListener("keyup", function(event){
@@ -78,22 +112,48 @@ attachEventListeners = function(){
       keyReleased(f4)
     }else if(key == 'Enter'){
       keyReleased(g4)
+    }else if(key == '1'){
+      keyReleased(a3sharp)
+    }else if(key == '2'){
+      keyReleased(b3sharp)
+    }else if(key == '3'){
+      keyReleased(c3sharp)
+    }else if(key == '4'){
+      keyReleased(d3sharp)
+    }else if(key == '6'){
+      keyReleased(f3sharp)
+    }else if(key == '7'){
+      keyReleased(g3sharp)
+    }else if(key == '8'){
+      keyReleased(c4sharp)
+    }else if(key == '9'){
+      keyReleased(d4sharp)
+    }else if(key == '0'){
+      keyReleased(f4sharp)
+    }else if(key == '-'){
+      keyReleased(g4sharp)
     }
   })
 }
+returnKeyClassName = function(key){
+  console.log(key.appearance.className)
+}
+
+startingColor = ""
 //helper functions
 keyPressed = function(key){
-  key.sound.play()
-  key.appearance.setAttribute('style', 'background-color: grey;')
+  console.log(key.appearance.className)
+  key.sound.play();
+  key.appearance.className == 'white key' ? key.appearance.setAttribute('style', 'background-color: grey;') : key.appearance.setAttribute('style', 'background-color: rgba(0, 0, 0, 0.8);')
 }
 keyReleased = function(key){
   key.sound.currentTime = 0;
   key.sound.pause();
-  key.appearance.setAttribute('style', 'background-color: white;')
+  key.appearance.className == 'white key' ? key.appearance.setAttribute('style', 'background-color: white;') : key.appearance.setAttribute('style', 'background-color: rgba(0, 0, 0, 1);')
 }
 
 playNote = function(key){
-  keyPressed(key)
+  keyPressed(key);
   setTimeout(function(){keyReleased(key)}, 200)
 }
 // AI songs
@@ -129,58 +189,59 @@ maryHadALittleLamb = function(){
   setTimeout(function(){playNote(c3)}, 10000)
 }
 chopSticks = function(){
-  setTimeout(function(){playNote(g4)}, 100)
-  setTimeout(function(){playNote(e4)}, 100)
-  setTimeout(function(){playNote(g4)}, 400)
-  setTimeout(function(){playNote(e4)}, 400)
-  setTimeout(function(){playNote(g4)}, 700)
-  setTimeout(function(){playNote(e4)}, 700)
-  setTimeout(function(){playNote(g4)}, 1000)
-  setTimeout(function(){playNote(e4)}, 1000)
+  setTimeout(function(){playNote(g4sharp)}, 100)
+  setTimeout(function(){playNote(d4sharp)}, 100)
+  setTimeout(function(){playNote(g4sharp)}, 400)
+  setTimeout(function(){playNote(d4sharp)}, 400)
+  setTimeout(function(){playNote(g4sharp)}, 700)
+  setTimeout(function(){playNote(d4sharp)}, 700)
+  setTimeout(function(){playNote(g4sharp)}, 1000)
+  setTimeout(function(){playNote(d4sharp)}, 1000)
 // da da da
 
-  setTimeout(function(){playNote(f4)},1300)
-  setTimeout(function(){playNote(d4)},1300)
-  setTimeout(function(){playNote(f4)},1650)
-  setTimeout(function(){playNote(d4)},1650)
+  setTimeout(function(){playNote(f4sharp)},1300)
+  setTimeout(function(){playNote(c4sharp)},1300)
+  setTimeout(function(){playNote(f4sharp)},1650)
+  setTimeout(function(){playNote(c4sharp)},1650)
  // do do
 
-  setTimeout(function(){playNote(c4)},2000)
-  setTimeout(function(){playNote(e4)},2000)
+  setTimeout(function(){playNote(d4sharp)},2000)
+  setTimeout(function(){playNote(g3sharp)},2000)
 
-  setTimeout(function(){playNote(c4)},2350)
-  setTimeout(function(){playNote(e4)},2350)
+  setTimeout(function(){playNote(d4sharp)},2350)
+  setTimeout(function(){playNote(g3sharp)},2350)
 // do do
 
-  setTimeout(function(){playNote(d4)},2700)
-  setTimeout(function(){playNote(g3)},2700)
+  setTimeout(function(){playNote(f3sharp)},2700)
+  setTimeout(function(){playNote(c4sharp)},2700)
 // duuuuhhhhh
 
-  setTimeout(function(){playNote(d4)}, 3250)
-  setTimeout(function(){playNote(g3)}, 3250)
+  setTimeout(function(){playNote(g3sharp)}, 3250)
+  setTimeout(function(){playNote(d3sharp)}, 3250)
 
-  setTimeout(function(){playNote(d4)}, 3550)
-  setTimeout(function(){playNote(g3)}, 3550)
+  setTimeout(function(){playNote(g3sharp)}, 3550)
+  setTimeout(function(){playNote(d3sharp)}, 3550)
 
-  setTimeout(function(){playNote(d4)}, 3850)
-  setTimeout(function(){playNote(g3)}, 3850)
+  setTimeout(function(){playNote(g3sharp)}, 3850)
+  setTimeout(function(){playNote(g3sharp)}, 3850)
   // da da da
 
-  setTimeout(function(){playNote(f3)}, 4150)
-  setTimeout(function(){playNote(c4)}, 4150)
+  setTimeout(function(){playNote(f3sharp)}, 4150)
+  setTimeout(function(){playNote(c3sharp)}, 4150)
 
-  setTimeout(function(){playNote(f3)},4500)
-  setTimeout(function(){playNote(c4)},4500)
+  setTimeout(function(){playNote(f3sharp)},4500)
+  setTimeout(function(){playNote(c3sharp)},4500)
 // do do
 
-  setTimeout(function(){playNote(e3)},4850)
-  setTimeout(function(){playNote(g3)},4850)
+  setTimeout(function(){playNote(d3sharp)},4850)
+  setTimeout(function(){playNote(b3sharp)},4850)
   //duhhhh
 }
 
 schedule= []//when the    night        has   come              and     the    land   is    dark     and          the           moon is  the only     light   we'll  see        oh    I    wont        be     a   fraaaaid        NO
 standByMe = 'E      G      A            E      G                C      D      E    D    C            C         D   E           C      E  D   C         D     D     C            E      G      A         E     G    A            G      E   D        D   E        D        D   C          C         E         D   C        C         E       D   C        E           D     C'
 twinkleTwinkle = 'C   C      G   G     A   A   G    F   F     E   E      D   D    C   G      G   F   F   E   E    D   G      G      F   F     E    E    D      C   C      G   G     A   A   G   F     F     E   E     D     D     C'
+kungFuFighting = ''
 // string songs
 
 playString = function(string, speed){
